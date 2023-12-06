0abc24aa4f5e896e5c30cc7cbb0b572912817cd3
Ola <cencora.aleksandra@gmail.com>
2023-12-06 11:34:43 +0100

    Add section about, add styles

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,15 +1,35 @@
 import SectionTitle from "../../common/SectionTitle/SectionTitle";
+import { Row, Col } from "react-bootstrap";
+import styles from './About.module.scss';
+import clsx from "clsx";
+import AboutText from './about.txt';
+import { useEffect, useState } from "react";
 
 const About = () => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        fetch(AboutText)
+          .then((response) => response.text())
+          .then((data) => setText(data))
+          .catch((error) => console.error('Error fetching about text:', error));
+    }, []);
+
     return (
-        <>
-        <SectionTitle title="o mnie" />
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        </>
+        <div className={clsx(styles.about, 'container-fluid')}>
+            <Row>
+                <Col md={6} xs={12} className={clsx(styles.aboutImg, 'order-md-1 order-1')}>
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/about.jpg`}
+                        alt="zdjęcie o mnie"
+                    />
+                </Col>
+                <Col md={6} xs={12} className={clsx(styles.aboutTxt, 'order-md-2 order-2')}>
+                    <SectionTitle title="o mnie" />
+                    <p>{text}</p>
+                </Col>
+            </Row>
+        </div>
     );
 };
 
