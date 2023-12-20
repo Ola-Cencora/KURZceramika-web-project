6a0f4ef9288669d6f200bc9f2b91a50541fba150
Ola <cencora.aleksandra@gmail.com>
2023-12-20 13:53:23 +0100

    Add newsletter form including validation, no styles, prettier Contact

--- src/components/features/Newsletter/Newsletter.js
+++ src/components/features/Newsletter/Newsletter.js
@@ -1,18 +1,45 @@
 import SectionTitle from "../../common/SectionTitle/SectionTitle";
 import Button from "../../common/Button/Button";
+import { Form } from "react-bootstrap";
+import { useState } from "react";
 
 const Newsletter = () => {
-    return (
-        <>
-        <SectionTitle title="newsletter" />
-        <Button content="zapisz się" />
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        <p>xyz</p>
-        </>
-    );
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <>
+      <SectionTitle title="newsletter" />
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Control required type="text" placeholder="Twoje imię" />
+          <Form.Control.Feedback type="invalid">
+            Halo! Jak Ci na imię? :)
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control required type="email" placeholder="Twój email" />
+          <Form.Control.Feedback type="invalid">
+            Halo! Pamiętaj o podaniu adresu email w formacie abc@xyz.com :)
+          </Form.Control.Feedback>
+        </Form.Group>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <Button type="submit" content="zapisz się" />
+      </Form>
+    </>
+  );
 };
 
 export default Newsletter;
