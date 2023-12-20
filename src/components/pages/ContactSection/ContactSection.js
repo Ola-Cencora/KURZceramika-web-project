b5165f4f982fc37519785c37be83116061951e55
Ola <cencora.aleksandra@gmail.com>
2023-12-20 14:24:45 +0100

    Add styles for wrapper ContactSection

--- src/components/pages/ContactSection/ContactSection.js
+++ src/components/pages/ContactSection/ContactSection.js
@@ -1,15 +1,23 @@
 import Newsletter from "../../features/Newsletter/Newsletter";
 import Contact from "../../features/Contact/Contact";
+import styles from "./ContactSection.module.scss";
+import { Row, Col } from "react-bootstrap";
 
 const ContactSection = () => {
   return (
-    <div>
-      <div id="contact">
-        <Contact />
-      </div>
-      <div id="newsletter">
-        <Newsletter />
-      </div>
+    <div className={styles.contactSection}>
+      <Row>
+        <Col lg={6} xs={12} className="order-lg-1 order-1">
+          <div id="contact">
+            <Contact />
+          </div>
+        </Col>
+        <Col lg={6} xs={12} className="order-lg-2 order-2">
+          <div id="newsletter">
+            <Newsletter />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
