c57b835b02127b966739234e974a23504c3824d5
Ola <cencora.aleksandra@gmail.com>
2023-12-25 16:20:58 +0100

    Add styles for about subpage

--- src/components/features/ScrollToTop/ScrollToTop.js
+++ src/components/features/ScrollToTop/ScrollToTop.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Button from "../../common/Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpLong } from "@fortawesome/free-solid-svg-icons";
+import styles from "./ScrollToTop.module.scss";
 
 const ScrollToTop = () => {
   const [showScrollTopButton, setShowScrollTopButton] = useState(false);
@@ -30,7 +31,7 @@ const ScrollToTop = () => {
   };
 
   return (
-    <div>
+    <div className={styles.scroll}>
       {showScrollTopButton && (
         <div onClick={scrollTop}>
           <Button content={<FontAwesomeIcon icon={faUpLong} />} variant="top" />
--- src/components/pages/AboutSubpage/AboutSubpage.js
+++ src/components/pages/AboutSubpage/AboutSubpage.js
@@ -1,19 +1,41 @@
 import { useEffect, useState } from "react";
 import AboutTextMore from "./about_more.txt";
+import styles from "./AboutSubpage.module.scss";
+import SectionTitle from "../../common/SectionTitle/SectionTitle";
+import Button from "../../common/Button/Button";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const AboutSubpage = () => {
-  const [text, setText] = useState("");
+  const [paragraphs, setParagraphs] = useState([]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     fetch(AboutTextMore)
       .then((response) => response.text())
-      .then((data) => setText(data))
+      .then((data) => {
+        const paragrapsArray = data.split("\n");
+        setParagraphs(paragrapsArray);
+      })
       .catch((error) => console.error("Error fetching about text:", error));
   }, []);
 
   return (
-    <div>
-      <p>{text}</p>
+    <div className={styles.aboutSubpage}>
+      <Container>
+        <div className={styles.aboutTxt}>
+          <SectionTitle title="o mnie" />
+          {paragraphs.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </div>
+        <Link to="/">
+          <Button content="wróć do strony głównej" />
+        </Link>
+      </Container>
     </div>
   );
 };
