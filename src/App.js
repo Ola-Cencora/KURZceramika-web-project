9bb208308a21d63e8e24efdb2296b50b86ec60c6
Ola <cencora.aleksandra@gmail.com>
2023-12-25 16:37:15 +0100

    Add not found page view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ScrollToTop from "./components/features/ScrollToTop/ScrollToTop";
 import Footer from "./components/views/Footer/Footer";
 import HomePage from "./components/pages/HomePage/HomePage";
 import AboutSubpage from "./components/pages/AboutSubpage/AboutSubpage";
+import NotFound from "./components/views/NotFound/NotFound";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutSubpage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/components/views/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import Button from "../../common/Button/Button";
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.scss";
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFound}>
+      <p>Ups... nie ma takiej strony!</p>
+      <Link to="/">
+        <Button content="wróć do strony głównej" />
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
