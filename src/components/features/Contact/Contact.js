d5dd08a50a20c2da86930eb0e972efd669ef3230
Ola <cencora.aleksandra@gmail.com>
2024-02-20 11:58:22 +0100

    Enable contact message to be send by formspree, change contact validation

--- src/components/features/Contact/Contact.js
+++ src/components/features/Contact/Contact.js
@@ -1,97 +1,55 @@
 import SectionTitle from "../../common/SectionTitle/SectionTitle";
 import Button from "../../common/Button/Button";
+import SuccessInfo from "../../views/SuccessInfo/SuccessInfo";
 import { Form } from "react-bootstrap";
 import styles from "./Contact.module.scss";
-import { useFormik } from "formik";
-import * as yup from "yup";
 import { Fade } from "react-awesome-reveal";
+import { useForm } from "@formspree/react";
 
 const Contact = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      request: "",
-    },
-
-    validationSchema: yup.object({
-      name: yup.string().required("Halo! Jak Ci na imię? :)"),
-      email: yup
-        .string()
-        .email("Halo! Pamiętaj o prawidłowym adresie email :)")
-        .required("Halo! Pamiętaj o prawidłowym adresie email :)"),
-      request: yup.string().required("Halo! Jakie masz pytanie? :)"),
-    }),
-
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+  const [state, handleSubmit] = useForm("xkndrpkl");
 
   return (
     <Fade cascade direction="up" triggerOnce>
       <SectionTitle title="kontakt" />
-      <Form noValidate onSubmit={formik.handleSubmit} className={styles.form}>
-        <Form.Group className="my-2">
-          <Form.Control
-            type="text"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            placeholder={
-              formik.touched.name && formik.errors.name
-                ? formik.errors.name
-                : "Twoje imię"
-            }
-            className={
-              formik.touched.name && formik.errors.name
-                ? styles.inputInvalid
-                : styles.input
-            }
-            onBlur={formik.handleBlur}
-          />
-        </Form.Group>
-        <Form.Group className="my-2">
-          <Form.Control
-            type="email"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            placeholder={
-              formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : "Twój email"
-            }
-            className={
-              formik.touched.email && formik.errors.email
-                ? styles.inputInvalid
-                : styles.input
-            }
-            onBlur={formik.handleBlur}
-          />
-        </Form.Group>
-        <Form.Group className="my-2">
-          <Form.Control
-            name="request"
-            value={formik.values.request}
-            onChange={formik.handleChange}
-            as="textarea"
-            rows={10}
-            placeholder={
-              formik.touched.request && formik.errors.request
-                ? formik.errors.request
-                : "Twoje pytanie"
-            }
-            className={
-              formik.touched.request && formik.errors.request
-                ? styles.inputInvalid
-                : styles.input
-            }
-            onBlur={formik.handleBlur}
-          />
-        </Form.Group>
-        <Button type="submit" content="wyślij" />
-      </Form>
+      {state.succeeded ? (
+        <SuccessInfo />
+      ) : (
+        <Form onSubmit={handleSubmit} className={styles.form}>
+          <Form.Group className="my-2">
+            <Form.Control
+              type="text"
+              name="name"
+              required
+              placeholder="Twoje imię"
+              autoComplete="off"
+              className={styles.input}
+            />
+          </Form.Group>
+          <Form.Group className="my-2">
+            <Form.Control
+              type="email"
+              name="email"
+              required
+              placeholder="Twój email"
+              autoComplete="off"
+              className={styles.input}
+            />
+          </Form.Group>
+          <Form.Group className="my-2">
+            <Form.Control
+              name="message"
+              as="textarea"
+              type="text"
+              required
+              rows={10}
+              placeholder="Twoje pytanie"
+              className={styles.input}
+            />
+          </Form.Group>
+          <Button type="submit" content="wyślij" />
+        </Form>
+      )}
     </Fade>
   );
 };
---
+++ src/components/views/SuccessInfo/SuccessInfo.js
@@ -0,0 +1,7 @@
+const SuccessInfo = () => (
+  <div className="text-center">
+    <p>Dzięki za wiadomość!</p>
+  </div>
+);
+
+export default SuccessInfo;
